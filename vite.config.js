06f9cfce67254ee0b71f9ea4ea7494f5b4c5835f
mugil24 <vaanmugil24@gmail.com>
2024-07-21 12:53:23 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/collegewebsitegit/', // This is the base URL of your GitHub Pages site
+  base: '/collegewebsite/', // This is the base URL of your GitHub Pages site
 });
